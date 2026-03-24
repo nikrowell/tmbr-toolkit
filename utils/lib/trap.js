e79da3706ea11b839966fd8baa7ff5fd2e953cc9
Nik Rowell <nik@nikrowell.com>
2026-03-23 20:33:47 -0500

    utils@2.14.3 - update trap() to re-evaluate focusables on each tab press

--- utils/lib/trap.js
+++ utils/lib/trap.js
@@ -1,30 +1,44 @@
 import { findAll } from './findAll.js';
 import { focusables } from './focusables.js';
+import { isEmpty } from './isEmpty.js';
 import { isFunction } from './isFunction.js';
+import { noop } from './noop.js';
 import { wrap } from './wrap.js';
 
 /**
  * Traps focus within an element for keyboard navigation
  *
  * @param {Element} el - container element
- * @param {Function} callback - optional function to filter/modify focusable elements
+ * @param {Function} callback - optional function to modify focusable elements (re-evaluated on each Tab press)
  * @returns {Function} cleanup function to restore previous focus
  */
 export function trap(el, callback) {
 
   const previous = document.activeElement;
+  const selector = focusables.join(',');
 
-  let elements = findAll(focusables.join(','), el);
-  if (isFunction(callback)) elements = callback(elements);
+  let trapped = query();
+  if (isEmpty(trapped)) return noop;
 
-  elements[0].focus();
+  trapped[0].focus();
   let index = 0;
+
+  function query() {
+    let elements = findAll(selector, el);
+    if (isFunction(callback)) elements = callback(elements);
+    return elements;
+  }
 
   function keydown(event) {
     if (event.key !== 'Tab') return;
     event.preventDefault();
-    index = wrap(event.shiftKey ? index - 1 : index + 1, elements);
-    elements[index].focus();
+
+    trapped = query();
+    if (isEmpty(trapped)) return;
+
+    index = trapped.indexOf(document.activeElement);
+    index = wrap(event.shiftKey ? index - 1 : index + 1, trapped);
+    trapped[index].focus();
   }
 
   window.addEventListener('keydown', keydown);
